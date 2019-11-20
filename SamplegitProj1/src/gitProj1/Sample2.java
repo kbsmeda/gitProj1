68475003780359b4d70c830ae75dd1445a9fa4ff
kkk <kavithawil@gmail.com>
2019-11-19 23:08:15 -0500

    added Sample2.java

---
+++ SamplegitProj1/src/gitProj1/Sample2.java
@@ -0,0 +1,15 @@
+package gitProj1;
+
+public class Sample2 {
+	//int x=0,y=0;
+	static int sum(int x, int y) {
+		return x+y;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(sum(2,5));
+		
+	}
+
+}
